25ed269f905c9c8ab3481fa277446f746ecfa03c
MorelK1 <kouhossounon.vianney@gmail.com>
2024-01-08 16:21:54 +0100

    `feat 👨‍💻` prediction

--- app/src/main/java/com/example/androidbraintumordetection/MainActivity.java
+++ app/src/main/java/com/example/androidbraintumordetection/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidbraintumordetection;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -12,6 +13,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.androidbraintumordetection.ml.Model;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
 
@@ -27,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageUtils.saveImageToStorage(this, R.drawable.test, "test.jpg");
+//        ImageUtils.saveImageToStorage(this, R.drawable.test, "test.jpg");
 
         selectBtn = findViewById(R.id.selectBtn);
         predictBtn = findViewById(R.id.predictBtn);
@@ -43,6 +50,43 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 10);
             }
         });
+
+        predictBtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                try {
+                    Model model = Model.newInstance(MainActivity.this);
+
+                    // Creates inputs for reference.
+                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+
+                    bitmap = Bitmap.createScaledBitmap( bitmap, 224, 224, true );
+
+                    inputFeature0.loadBuffer(TensorImage.fromBitmap(bitmap).getBuffer());
+
+                    // Runs model inference and gets result.
+                    Model.Outputs outputs = model.process(inputFeature0);
+
+                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+                    result.setText(getMax(outputFeature0.getFloatArray()) + "");
+
+                    // Releases model resources if no longer used.
+                    model.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    int getMax(float[] arr){
+        int max = 0;
+        for (int i=0; i<arr.length; i++){
+            if (arr[i] > arr[max]) max=i;
+        }
+        return max;
     }
 
     @Override
